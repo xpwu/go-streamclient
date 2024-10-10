4489cb7cfd889bdbb4a5e8dbd6c39d0375303226
xpwu <diodexp@foxmail.com>
2024-10-10 15:16:06 +0800

    fix bug: 'ws://' prefix error

--- websocketc/client.go
+++ websocketc/client.go
@@ -1,24 +1,23 @@
 package websocketc
 
 import (
-  "context"
-  "github.com/xpwu/go-streamclient/fakehttpc"
-  "strings"
+	"context"
+	"github.com/xpwu/go-streamclient/fakehttpc"
+	"strings"
 )
 
 type Client struct {
-  fakehttpc.Client
+	fakehttpc.Client
 }
 
 func New(ctx context.Context, addr string) *Client {
-  if !strings.HasPrefix(addr, "://") {
-    addr = "ws://" + addr
-  }
+	if !strings.HasPrefix(addr, "ws://") && !strings.HasPrefix(addr, "wss://") {
+		addr = "ws://" + addr
+	}
 
-  ret := &Client{
-    Client: *fakehttpc.NewClient(ctx, addr, &connector{}),
-  }
+	ret := &Client{
+		Client: *fakehttpc.NewClient(ctx, addr, &connector{}),
+	}
 
-  return ret
+	return ret
 }
-
